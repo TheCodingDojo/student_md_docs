be527817bb52e9454149496c620f6ff13ea181b9
Neil M <neilm813@gmail.com>
2020-04-11 18:32:59 -0700

    format

--- CA-OC/JavaScript/callbackExample.js
+++ CA-OC/JavaScript/callbackExample.js
@@ -1,9 +1,11 @@
 function callFriend(yourName, friendsName, callMeBack) {
   console.log(`Ring ring, calling ${friendsName}`);
-  console.log(`${friendsName}: Hey ${yourName}, let me call you back real quick!`);
+  console.log(
+    `${friendsName}: Hey ${yourName}, let me call you back real quick!`
+  );
 
   setTimeout(function () {
-    callMeBack(friendsName)
+    callMeBack(friendsName);
   }, 3000);
 }
 
@@ -11,4 +13,4 @@ function receiveCallBack(callersName) {
   console.log(`Ring ring, ${callersName} is calling you back.`);
 }
 
-callFriend("Your Name", "Friend's Name", receiveCallBack)
+callFriend("Your Name", "Friend's Name", receiveCallBack);
